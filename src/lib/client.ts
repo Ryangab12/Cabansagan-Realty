f363b37ece56ba580b55e37ecf61eb57915d298a
Padre <nonoyjpgerona@gmail.com>
2025-03-20 19:55:45 +0800

    update the client.ts

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,9 +1,11 @@
+import { createClient as createSupabaseClient } from "@supabase/supabase-js";
 import { createBrowserClient } from "@supabase/ssr";
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createSupabaseClient(supabaseUrl, supabaseAnonKey);
+
 export function createClient() {
-  // Create a supabase client on the browser with project's credentials
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
+  return createBrowserClient(supabaseUrl, supabaseAnonKey);
 }
