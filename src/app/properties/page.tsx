311777875901ee4dd86ec863a95f28d0885abea7
Padre <124707840+Padre108@users.noreply.github.com>
2025-03-23 00:01:23 +0800

    Revert "added functional frontend filter system"

--- src/app/properties/page.tsx
+++
@@ -1,6 +0,0 @@
-import PropertyList from "../../components/frontend/propertylisting"; // Adjust path as needed
-import React from "react";
-
-export default function PropertiesPage() {
-  return <PropertyList />;
-}
